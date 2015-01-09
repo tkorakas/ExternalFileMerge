d0ee90979f2221811f7d2f72a41df7f7a7031f44
Thanos <thanoskorakas@gmail.com>
2015-01-09 12:54:40 +0200

    crf error fixes

--- createRandomFIle/src/crf/MAIN.java
+++ createRandomFIle/src/crf/MAIN.java
@@ -10,7 +10,8 @@ public class MAIN {
 				"C:\\Users\\Thanos\\workspace\\Merger\\file.txt", "rw");
 		for(int i=0;i<10240;i+=4){
 			file.seek(i);
-			System.out.println(file.readInt());
+			file.writeInt((int) (Math.random()*1000));// comment and uncomment print to print file change *1000 to change random range
+//			System.out.println(file.readInt()); // show file
 		}
 		file.close();
 	}
